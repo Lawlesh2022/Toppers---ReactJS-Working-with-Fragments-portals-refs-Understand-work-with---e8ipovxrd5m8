0596f67194f39f02804f7a95fc0e2319e233784c
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-27 08:24:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -51,10 +51,10 @@ class App extends Component {
 				   				//if the percent >= 90 else className = "" 
 				   				<div key={index} className={}>
 					   				<div className="name">
-					   					 //Write code to display name
+										{item.name}
 					   				</div>
 					   				<div className="percent">
-					   					 //Write code to display percentage upto 2 decimal places
+										{item.percent}
 					   				</div>
 				   				</div>
 				   				);
